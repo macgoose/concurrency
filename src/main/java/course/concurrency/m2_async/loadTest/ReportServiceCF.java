a41b8d79e78cab1687162ec970ea4da6c64fcebd
macgoose <gusakmachine@yandex.ru>
2026-08-12 13:03:48 +0300

    l.2.3

--- src/main/java/course/concurrency/m2_async/loadTest/ReportServiceCF.java
+++ src/main/java/course/concurrency/m2_async/loadTest/ReportServiceCF.java
@@ -4,11 +4,12 @@ import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 
 public class ReportServiceCF {
 
-    private ExecutorService executor = ForkJoinPool.commonPool();
+    private ExecutorService executor = Executors.newFixedThreadPool(16);
 
     private LoadGenerator loadGenerator = new LoadGenerator();
 
