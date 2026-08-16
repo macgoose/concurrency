3eeb31b5027aaf2b38ff67bdd2718b395425ad92
macgoose <gusakmachine@yandex.ru>
2026-08-16 13:22:51 +0300

    feat(l.3.2): counter

--- src/main/java/course/concurrency/m3_shared/Counter.java
+++ src/main/java/course/concurrency/m3_shared/Counter.java
@@ -2,16 +2,36 @@ package course.concurrency.m3_shared;
 
 public class Counter {
 
-    public static void first() {
+    private static final Object LOCK = new Object();
+    private static int currPhase = 1;
 
+    public static void syncPrint(int phase) {
+        synchronized (LOCK) {
+            while (currPhase != phase) {
+                try {
+                    LOCK.wait();
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+            }
+
+            System.out.println(phase);
+            currPhase++;
+            LOCK.notifyAll();
+        }
+    }
+
+    public static void first() {
+        syncPrint(1);
     }
 
     public static void second() {
-
+        syncPrint(2);
     }
 
     public static void third() {
-
+        syncPrint(3);
     }
 
     public static void main(String[] args) {
